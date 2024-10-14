a502b562912765a7bae095df01e29e1b89208ca9
Matthew Rowland <contact@mathhulk.com>
2024-10-14 12:08:23 -0700

    Finish baseline model

--- semantic_search/__init__.py
+++ semantic_search/__init__.py
@@ -1,27 +1,31 @@
 from flask import Flask, jsonify, request
+import importlib
+import os
 
 app = Flask(__name__)
 
-# Models
-models = ["model1", "model2"]
-# TODO: Dyanmically import modules from models folder
+loaded_models = {
+    # "model_name": model
+}
+
+for file in os.listdir("semantic_search/models"):
+    if file.endswith(".py"):
+        model_name = file.replace(".py", "")
+        model = importlib.import_module(f"semantic_search.models.{model_name}")
+        loaded_models[model_name] = model
 
 
 # Routes
 @app.route("/models", methods=["GET"])
 def get_models():
-    return jsonify(models)
+    return jsonify(list(loaded_models.keys()))
 
 
 @app.route("/courses", methods=["GET"])
 def get_items():
     query = request.args.get("query", type=str)
-    model = request.args.get("model", type=str)
+    input_models = request.args.get("model", type=str)
     topK = request.args.get("topK", default=5, type=int)
-
-    # Check if a valid model was provided
-    if model not in models:
-        return jsonify("Invalid model"), 400
 
     # Check if a query was provided
     if query is None:
@@ -30,9 +34,27 @@ def get_items():
     # Limit topK to 100
     topK = min(topK, 100)
 
-    # TODO: Call the model to get the results
+    selected_models = [] if input_models is not None else list(loaded_models.keys())
 
-    return jsonify(None)
+    # Filter the input models
+    if input_models is not None:
+        for model_name in input_models.split(","):
+            if model_name in loaded_models:
+                selected_models.append(model_name)
+            else:
+                return jsonify(f"Model {model_name} not found"), 400
+
+    results = []
+
+    for model_name in selected_models:
+        result = {
+            "model": model_name,
+            "courses": loaded_models[model_name].getCourses(query, topK),
+        }
+
+        results.append(result)
+
+    return jsonify(results)
 
 
 if __name__ == "__main__":
